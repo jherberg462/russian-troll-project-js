7dd99ea4059f4fb58267ed84a51fd3e62859c9bb
Rich-Hamilton88 <53978255+Rich-Hamilton88@users.noreply.github.com>
2020-02-01 07:41:04 -0500

    update js file

--- static/js/app.js
+++ static/js/app.js
@@ -1,100 +1,243 @@
+function unpack(rows, index) {
+    return rows.map(function(row) {
+      return row[index];
+    });
+  }
 
+function getFollowing() {
+    var start = '2013-01-01'
+    var end = '2017-11-29'
+    var url3 = `http://localhost:5000/api/data/following/${start}/${end}`
+    
+    d3.json(url3).then(function(data4) {
+        console.log(data4[0]);
+
+        var following = data4
+        var avg_following = following.map(average_num_following => average_num_following.dates);
+        console.log(avg_following);
+
+        var trace3 = {
+            x: following,
+            y: avg_following,
+            mode: 'lines+markers',
+            marker: {
+                color: 'rgb(128, 0, 128)',
+                size: 8
+            },
+            line: {
+                color: 'rgb(128, 0, 128)',
+                width: 1
+            }
+        };
+    
+        var plotdata3 = [trace3];
+        
+        
+    Plotly.newPlot('line2', plotdata3, {responsive: true});
+
+        
+    });
+}
+
+getFollowing();
+
+
+function getFollowers() {
+    var start = '2013-01-01'
+    var end = '2017-11-29'
+    var url4 = `http://localhost:5000/api/data/followers/${start}/${end}`
+    
+    d3.json(url4).then(function(data3) {
+        console.log(data3[0]);
+
+        var followers = data3
+
+        var avg_followers = followers.map(average_num_of_followers => average_num_of_followers.dates);
+        console.log(avg_followers);
+
+         var trace2 = {
+            x: followers,
+            y: avg_followers,
+            mode: 'lines+markers',
+            marker: {
+                color: 'rgb(128, 0, 128)',
+                size: 8
+            },
+            line: {
+                color: 'rgb(128, 0, 128)',
+                width: 1
+            }
+        };
+    
+        var plotdata2 = [trace2];
+        
+        
+    Plotly.newPlot('line', plotdata2, {responsive: true});
+
+        
+    });
+}
+
+getFollowers();
+
+
+function getDates() {
+    var start = '2013-01-01'
+    var end = '2017-11-29'
+    var url5 = `http://localhost:5000/api/data/tweet_len/${start}/${end}`
+    
+    d3.json(url5).then(function(data2) {
+        console.log(data2[0]);
+
+        var tweetdates = data2
+
+        var tweetdate = tweetdates.map(date => date.dates);
+        var tweetLen = tweetdates.map(date => date.tweet_length);
+        console.log(tweetdate);
+        console.log(tweetLen);
+ 
+
+        var trace1 = {
+            x: tweetdate,
+            y: tweetLen,
+            mode: 'markers',
+            type: 'scatter'
+          };
   
-  //   var trace1 = {
-  //     x: content,
-  //     y: region,
-  //     text: author,
-  //     marker: {
-  //       color: 'rgb(142,124,195)'},
-  //     type: "bar",
-  //     orientation: "h",
-  // };
-
-  //     var data = [trace1];
-
-  //     var layout = {
-  //         title: "Tweets",
-  //         yaxis: {
-  //             tickmode: "linear",
-  //         },
-  //         margin: {
-  //             l: 100,
-  //             r: 100,
-  //             t: 100,
-  //             b: 30
-  //         }
-  // //       // paper_bgcolor: 'rgba(0,0,0,0)',
-  // //       // plot_bgcolor: 'rgba(0,0,0,0)'
-  //   };
-
-  //   Plotly.newPlot("bar", data, layout);
-
-
-
-
-
-  // var trace2 = {
-  //     x: [],
-  //     y: [],
-  //     mode: 'markers',
-  //     marker: {
-  //       color: 'rgb(219, 64, 82)',
-  //       size: 12
-  //     }
-  //   };
-    
-  // var trace3 = {
-  //     x: [],
-  //     y: [],
-  //     mode: 'lines',
-  //     line: {
-  //       color: 'rgb(55, 128, 191)',
-  //       width: 3
-  //     }
-  //   };
-    
-  // var trace4 = {
-  //     x: [],
-  //     y: [],
-  //     mode: 'lines+markers',
-  //     marker: {
-  //       color: 'rgb(128, 0, 128)',
-  //       size: 8
-  //     },
-  //     line: {
-  //       color: 'rgb(128, 0, 128)',
-  //       width: 1
-  //     }
-  //   };
-    
-  //   var data2 = [trace2, trace3, trace4];
-    
-  //   var layout = {
-  //     title: 'Tweets'
-  //   };
-    
-  //   Plotly.newPlot('line', data2, layout);
-
-
-
-  // var trace5 = {
-  //   x: [],
-  //   y: [],
-  //   text: ['A<br>size: 40', 'B<br>size: 60', 'C<br>size: 80', 'D<br>size: 100'],
-  //   mode: 'markers',
-  //   marker: {
-  //     color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
-  //     size: [40, 60, 80, 100]
-  //   }
-  // };
+        // create data variable
+        var plotdata = [trace1];
   
-  // var data3 = [trace5];
+        // create the bar plot
+        Plotly.newPlot("scatter", plotdata, {responsive: true});
+         
+    var startdate = $("#datepickerstart").datepicker( "option", "dateFormat", "yy-mm-dd" );
+    var enddate = $("#datepickerend").datepicker( "option", "dateFormat", "yy-mm-dd" );
+
+
+ 
+    function handleSubmit() {
+        d3.event.preventDefault();
+        var submitdate = d3.select("#datepickerend").node().value;
+        console.log(submitdate);
+   
+    }
+
+    // // Add event listener for submit button
+    d3.select("#submit").on("click", handleSubmit);
+    });
+}
+
+getDates();
+
+
+function getInfo() {
     
-  // var layout = {
-  //     title: 'Tweets',
-  //     showlegend: false,
-  //     height: 600,
-  //     width: 600
-  //   };
+    var url2 = `http://localhost:5000/api/data/account_dashbord/CHEYENNE_ENG`;
+
+    d3.json(url2).then(function(data1) {
+        console.log(data1[0]);
+
+        var tweetdata = data1
+     
+        var accountCategory = tweetdata.map(info => info.account_category);
+        var earliestTweet = tweetdata.map(info => info.earliest_tweet);
+        var latestTweet = tweetdata.map(info => info.latest_tweet);
+        var numInteractionpertweet = tweetdata.map(info => info.num_interactions_per_tweet);
+        var numTweets = tweetdata.map(info => info.number_of_tweets);
+        var twitterHandle = tweetdata.map(info => info.twitter_handle);
+        buildTable(accountCategory, earliestTweet, latestTweet, numInteractionpertweet, numTweets, twitterHandle);
+    });
+}
   
-  // Plotly.newPlot('bubble', data3, layout);
+
+function buildTable(accountCategory, earliestTweet, latestTweet, numInteractionpertweet, numTweets, twitterHandle) {
+    var tweetInfo = d3.select('#sample-tweetdata');
+    tweetInfo.html('')
+    tweetInfo.append("p").text("Account Category: " + accountCategory)
+    tweetInfo.append("p").text("Earliest Tweet: " + earliestTweet)
+    tweetInfo.append("p").text('Latest Tweet: ' + latestTweet)
+    tweetInfo.append('p').text('# of Interaction per Tweet: ' + numInteractionpertweet)
+    tweetInfo.append('p').text("# of Tweets: " + numTweets)
+    tweetInfo.append('p').text('Twitter Handle: ' + twitterHandle)
+    
+}
+
+getInfo();
+
+function getPlot() {
+
+    var dropdown = d3.select("#selDataset");
+
+    var url = `http://localhost:5000/api/data/accounts`
+
+     d3.json(url).then(function(data) {
+        console.log(data[0]);
+
+
+        data[0].twitter_handle.forEach(function(name) {
+            dropdown.append("option").text(name).property("value");
+
+    }); 
+
+    });
+}
+
+getPlot();
+
+
+d3.selectAll("#selDataset").on("change", updateData);
+
+function updateData() {
+
+    
+
+    d3.json(url2).then(function(data1) {
+        // console.log(data1[0]);
+
+    var tweetdata = data1
+
+    
+
+    d3.json(url).then(function(data) {
+        // console.log(data[0]);
+
+    var selection = d3.select("#selDataset");
+    var selectedData = selection.property('value');
+    function filterTweetData(abc){
+        return abc.id == selectedData
+    }
+    var filteredID = tweetdata.filter(filterTweerData)
+
+    var filteredaccountCategory = filteredID.map(id => id.account_category);
+    var filteredearliestTweet = filteredID.map(id => id.earliest_tweet);
+    var filteredlatestTweet = filteredID.map(id => id.latest_tweet);
+    var filterednumInteractionpertweet = filteredID.map(id => id.num_interactions_per_tweet);
+    var filterednumTweets = filteredID.map(id => id.number_of_tweets);
+    var filteredtwitterHandle = filteredID.map(id => id.twitter_handle);
+
+    function filterTweetid(){
+        return data[0] == selectedData
+    } 
+
+    var tweetInfo = d3.select('#sample-tweetdata');
+    tweetInfo.html('')
+    tweetInfo.append("p").text("Account Category: " + filteredaccountCategory)
+    tweetInfo.append("p").text("Earliest Tweet: " + filteredearliestTweet)
+    tweetInfo.append("p").text('Latest Tweet: ' + filteredlatestTweet)
+    tweetInfo.append('p').text('# of Interaction per Tweet: ' + filterednumInteractionpertweet)
+    tweetInfo.append('p').text("# of Tweets: " + filterednumTweets)
+    tweetInfo.append('p').text('Twitter Handle: ' + filteredtwitterHandle)
+
+
+    });
+
+    });
+ }
+// function optionChanged() {
+//     getInfo();
+//     getPlot();
+//     getDates();
+//     getfollowers();
+//     getfollowing();
+// }
+
