52e230ff70eb5482412d3e72cd4f1a9bdc75217f
jherberg462 <jherberg88@me.com>
2021-04-25 17:12:24 -0400

    fix port setting on api

--- app.py
+++ app.py
@@ -317,7 +317,8 @@ def account_dashbord(account):
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 # In[ ]:
